94dd55f0fca3171121f1f20e896d556443e8bc97
prajapati bansi <prajapatibansi1510@gmail.com>
2026-05-19 19:59:50 +0530

    react query complete

--- src/components/FetchRq.jsx
+++ src/components/FetchRq.jsx
@@ -1,24 +1,41 @@
-import { useQuery } from "@tanstack/react-query";
-import React from "react";
-import { fetchDataAPI } from "../services/api";
+import {
+  keepPreviousData,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import React, { useState } from "react";
+import { deletePostAPI, fetchDataAPI, updatePostAPI } from "../services/api";
 import { NavLink } from "react-router-dom";
 
 const FetchRq = () => {
-  const fetchData = async () => {
-    try {
-      const res = await fetchDataAPI();
-      return res.status === 200 ? res.data : [];
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  };
+  const [pageNo, setPageNo] = useState(0);
+
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchData,
-    gcTime: 2000,
+    queryKey: ["posts", pageNo],
+    queryFn: () => fetchDataAPI(pageNo),
+    placeholderData: keepPreviousData,
   });
-  console.log(error);
+  const QueryClient = useQueryClient();
+  const deleteMutation = useMutation({
+    mutationFn: (id) => deletePostAPI(id),
+    onSuccess: (data, id) => {
+      QueryClient.setQueryData(["posts", pageNo], (curData) => {
+        return curData?.filter((post) => post.id !== id);
+      });
+    },
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: (id) => updatePostAPI(id),
+    onSuccess: (data, id) => {
+      QueryClient.setQueriesData(["posts", pageNo], (posts) => {
+        return posts.map((p) => {
+          return p.id === id ? { ...p, title: "this post updated" } : p;
+        });
+      });
+    },
+  });
   if (isLoading) return <p>Loading data</p>;
   if (isError) return <p>Error : {error.message}</p>;
 
@@ -35,10 +52,26 @@ const FetchRq = () => {
                   <p>{title}</p>
                   <p>{body}</p>
                 </NavLink>
+                <button onClick={() => deleteMutation.mutate(id)}>
+                  Delete
+                </button>
+                <button onClick={() => updateMutation.mutate(id)}>
+                  Update
+                </button>
               </li>
             );
           })}
         </ul>
+        <div className="pagination-section container">
+          <button
+            disabled={pageNo === 0 ? true : false}
+            onClick={() => setPageNo((prev) => prev - 3)}
+          >
+            Prev
+          </button>
+          <p>{pageNo / 3 + 1}</p>
+          <button onClick={() => setPageNo((prev) => prev + 3)}>Next</button>
+        </div>
       </div>
     </>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -7,6 +7,51 @@ const api = axios.create({
 })
 
 
-export const fetchDataAPI = async () => {
-  return api.get("/posts");
+export const fetchDataAPI = async (pageno) => {
+  try {
+    const res = await api.get(`/posts?_start=${pageno}&_limit=3`);
+
+    return res.status === 200 ? res.data : [];
+  } catch (error) {
+    console.log(error)
+  }
 }
+
+export const fetchSinglePost = async (id) => {
+
+  try {
+    const res = await api.get(`/posts/${id}`);
+    return res.status == 200 ? res.data : []
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+export const deletePostAPI = async (id) => {
+  try {
+    return api.delete(`/posts/${id}`);
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+export const updatePostAPI = async (id) => {
+  try {
+    return api.put(`/posts/${id}`);
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+export const fetchUsers = async ({ pageParam = 1 }) => {
+  try {
+    const res = await axios.get(
+      `https://api.github.com/users?per_page=10&page=${pageParam}`
+    );
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
